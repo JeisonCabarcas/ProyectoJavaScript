aeca98425eee798ecb25f5f2ea1269be7cb19d82
Jeison Cabarcas <jeison.cabarcas@alteru.co>
2021-06-09 12:45:47 -0500

    ejercicio # 2 terminado - usuarios y contraseñas

---
+++ assets/js/script2.js
@@ -0,0 +1,71 @@
+let usuarios = [
+    {
+        usuarioNombre: "jeison",
+        contraseña: "alteru1"
+    },
+
+    {
+        usuarioNombre: "johan",
+        contraseña: "alteru2"
+    },
+
+    {
+        usuarioNombre: "catalina",
+        contraseña: "alteru3"
+    },
+
+    {
+        usuarioNombre: "karina",
+        contraseña: "alteru4"
+    },
+
+    {
+        usuarioNombre: "invitado",
+        contraseña: "alteru5"
+    }
+];
+let cuenta = [
+    {
+        nombre: "Jeison",
+        saldo: "1.232.000"
+    },
+    {
+        nombre: "Johan",
+        saldo: "4.583.474"
+    },
+    {
+        nombre: "Catalina",
+        saldo: "7.485.485"
+    },
+    {
+        nombre: "Karina",
+        saldo: "9.586.384"
+    },
+    {
+        nombre: "Invitado",
+        saldo: "999.999.999"
+    }
+];
+function loggin(user, pass) {
+    let tagUser = 0;
+    let tagID = 0;
+    for (let i = 0; i < 5; i++) {
+        tagUser++;
+        if (user.toLowerCase() === usuarios[i].usuarioNombre && pass === usuarios[i].contraseña) {
+            tagID = tagUser-1;
+            alert("Hola " + cuenta[tagID].nombre + " su saldo es: " + cuenta[tagID].saldo);
+            console.log("Hola " + cuenta[tagID].nombre + " su saldo es: " + cuenta[tagID].saldo);
+        }else if(tagUser>=5){
+            alert("Ops, No te encuentras registrado en nuestro sistema");
+            console.log("Ops, No te encuentras registrado en nuestro sistema");
+        }
+    }
+   
+};
+
+
+let usuarioPrompt = prompt("Ingrese su usuario");
+let contraseñaPrompt = prompt("Ingrese su contraseña");
+
+loggin(usuarioPrompt, contraseñaPrompt);
+//console.log(usuarioPrompt.toLowerCase());//
