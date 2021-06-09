f388a0055db8be9cac6ec7ba5426f3c0730cb4e8
Jeison Cabarcas <jeison.cabarcas@alteru.co>
2021-06-09 11:00:24 -0500

    Terminaciond de hola mundo - comienzo de ejercicio # 2

---
+++ assets/js/script1.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Ingresa tu nombre");
+let apellido = prompt("Ingresa tu apellido");
+
+function saludo(nom,ape){
+alert("Recibe un especial saludo desde la consola")
+console.log("Hola "+nom+" "+ape+" Bienvenido al mundo de la programacion en Javascript")
+};
+
+saludo(nombre,apellido);
